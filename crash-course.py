25c8f279dcf123b9068d6793db4a29c0931f2795
Nory-Dush <h0n0r4bl3dush1nk4@gmail.com>
2026-09-10 16:26:41 +0000

    crashcourse day 2 complete

--- crash-course.py
+++ crash-course.py
@@ -1,4 +1,8 @@
-# comment   
+
+import math
+
+# comment 
+  
 
 # here
 # is
@@ -33,3 +37,50 @@ e -= 7
 e += 12
 
 print(f"e is NOW equal to {e}")
+
+# COMPARISONS (booleans, which always return True of False)
+
+#  <   >    <=   >=    ==    !=
+
+print(5 <=  5)
+print(7 == 4)
+print(1 != 2)
+
+isEqual = "Yes" != "YES"
+print(isEqual)
+
+# LOGICAL OPERATORS
+# In order of precedence: not   and   or
+
+f = False
+t = True
+
+#predict output. DOn't run!
+print(not f) # True
+print( f and t) #False
+print(f or t) #True
+print (f or t and not f) # True
+
+# CASTING ()
+
+
+g = int(5.9)
+print(g)
+
+# STRINGS
+
+s1 = "Goodnight" 
+s2 = " and "
+s3 = "Goodbye"
+end = s1 + s2 + s3 # concatenation with +
+end += ", Cowboy."
+
+print(end + "\n")
+
+# MATH LIBRARY
+
+print(math.sqrt(14))
+print(math.ceil(3.65))
+print(math.floor(8.94))
+print(math.pow(2,4))
+
